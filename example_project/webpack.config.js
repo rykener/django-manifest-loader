18e7a8de90a997a5529c4963e4d06d2d4554d687
Elliot Charney <echarney@pagerduty.com>
2020-10-04 15:55:42 -0700

    fix paths

--- example_project/webpack.config.js
+++ example_project/webpack.config.js
@@ -3,13 +3,13 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  entry: './django_project/frontend/src/index.js',
+  entry: './frontend/src/index.js',
   plugins: [
       new CleanWebpackPlugin(),  // removes outdated assets from the output dir
       new ManifestPlugin(),  // generates the required manifest.json file
   ],
   output: {
     filename: '[name].[contenthash].js',  // renames files from example.js to example.8f77someHash8adfa.js
-    path: path.resolve(__dirname, 'django_project/dist'),
+    path: path.resolve(__dirname, 'dist'),
   },
 };
