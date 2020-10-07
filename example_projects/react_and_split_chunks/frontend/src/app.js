be1df3c2cbdb8ff37e208c4e998d2074c9084c3a
Elliot Charney <echarney@pagerduty.com>
2020-10-07 10:53:50 -0700

    show how to use with react and split chunks

---
+++ example_projects/react_and_split_chunks/frontend/src/app.js
@@ -0,0 +1,7 @@
+import {render} from "react-dom";
+import React from "react";
+
+import App from "./components/App";
+
+const container = document.getElementById("app");
+render(<App />, container);
